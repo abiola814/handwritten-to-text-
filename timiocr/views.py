504cc11daaa029d22dba4e064d407c3a35a15020
techsnipersss <bigcashback1234@gmail.com>
2022-04-24 21:00:30 +0100

    jsjjj

--- timiocr/views.py
+++ timiocr/views.py
@@ -9,82 +9,82 @@ import requests
 import time
 from io import BytesIO
 from main.models import Register
-from docx import Document
+# from docx import Document
 
-missing_env = False
-# Add your Computer Vision subscription key and endpoint to your environment variables.
+# missing_env = False
+# # Add your Computer Vision subscription key and endpoint to your environment variables.
 
-doc = Document()
+# doc = Document()
 
 def home(request):
-    if request.method == 'POST' and request.FILES['upload']:
-        upload = request.FILES['upload']
-        fss = FileSystemStorage()
-        file = fss.save(upload.name, upload)
-        file_url = fss.url(file)
-        endpoint = 'https://handwritten-to-text2.cognitiveservices.azure.com'
-        subscription_key = '113fad96017f4f6891354e104c42daad'
-        text_recognition_url = endpoint + "/vision/v3.1/read/analyze"
+#     if request.method == 'POST' and request.FILES['upload']:
+#         upload = request.FILES['upload']
+#         fss = FileSystemStorage()
+#         file = fss.save(upload.name, upload)
+#         file_url = fss.url(file)
+#         endpoint = 'https://handwritten-to-text2.cognitiveservices.azure.com'
+#         subscription_key = '113fad96017f4f6891354e104c42daad'
+#         text_recognition_url = endpoint + "/vision/v3.1/read/analyze"
 
-# Set image_url to the URL of an image that you want to recognize.
-        image_url = 'https://handtotext2.herokuapp.com/media/'+ upload.name
-        headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+# # Set image_url to the URL of an image that you want to recognize.
+#         image_url = 'https://handtotext2.herokuapp.com/media/'+ upload.name
+#         headers = {'Ocp-Apim-Subscription-Key': subscription_key}
 
-        data = {'url': image_url}
-        response = requests.post(
-            text_recognition_url, headers=headers, json=data)
-        response.raise_for_status()
+#         data = {'url': image_url}
+#         response = requests.post(
+#             text_recognition_url, headers=headers, json=data)
+#         response.raise_for_status()
 
-        # Extracting text requires two API calls: One call to submit the
-        # image for processing, the other to retrieve the text found in the image.
+#         # Extracting text requires two API calls: One call to submit the
+#         # image for processing, the other to retrieve the text found in the image.
 
-        # Holds the URI used to retrieve the recognized text.
-        operation_url = response.headers["Operation-Location"]
+#         # Holds the URI used to retrieve the recognized text.
+#         operation_url = response.headers["Operation-Location"]
 
-        # The recognized text isn't immediately available, so poll to wait for completion.
-        analysis = {}
-        poll = True
-        while (poll):
-            response_final = requests.get(
-                response.headers["Operation-Location"], headers=headers)
-            analysis = response_final.json()
+#         # The recognized text isn't immediately available, so poll to wait for completion.
+#         analysis = {}
+#         poll = True
+#         while (poll):
+#             response_final = requests.get(
+#                 response.headers["Operation-Location"], headers=headers)
+#             analysis = response_final.json()
             
-            print(json.dumps(analysis, indent=4))
+#             print(json.dumps(analysis, indent=4))
 
-            time.sleep(1)
-            if ("analyzeResult" in analysis):
-                poll = False
-            if ("status" in analysis and analysis['status'] == 'failed'):
-                poll = False
+#             time.sleep(1)
+#             if ("analyzeResult" in analysis):
+#                 poll = False
+#             if ("status" in analysis and analysis['status'] == 'failed'):
+#                 poll = False
 
-        polygons = []
-        if ("analyzeResult" in analysis):
-            # Extract the recognized text, with bounding boxes.
-            polygons = [(line["boundingBox"], line["text"])
-                        for line in analysis["analyzeResult"]["readResults"][0]["lines"]]
+#         polygons = []
+#         if ("analyzeResult" in analysis):
+#             # Extract the recognized text, with bounding boxes.
+#             polygons = [(line["boundingBox"], line["text"])
+#                         for line in analysis["analyzeResult"]["readResults"][0]["lines"]]
 
-        # Display the image and overlay it with the extracted text.
+#         # Display the image and overlay it with the extracted text.
     
-        final=''
-        for polygon in polygons:
-            vertices = [(polygon[0][i], polygon[0][i+1])
-                        for i in range(0, len(polygon[0]), 2)]
-            text = polygon[1]
-            wordss=str(text)
+#         final=''
+#         for polygon in polygons:
+#             vertices = [(polygon[0][i], polygon[0][i+1])
+#                         for i in range(0, len(polygon[0]), 2)]
+#             text = polygon[1]
+#             wordss=str(text)
 
-            final=final+ wordss
-            hs=open("media/specs/hesjtigs.txt","a")
-            hs.write(text + "\n")
-            hs.close()
-        files='/media/'+ upload.name
+#             final=final+ wordss
+#             hs=open("media/specs/hesjtigs.txt","a")
+#             hs.write(text + "\n")
+#             hs.close()
+#         files='/media/'+ upload.name
 
-        g=Register.objects.create(text="specs/hesjtigs.txt")
-        h=Register.objects.get(text="specs/hesjtigs.txt")
-        print(h.text.url)
-        with open("media/specs/hesjt.txt", 'r', encoding='utf-8') as openfile:
-            line = openfile.read()
-            doc.add_paragraph(line)
-            doc.save('file' + ".docx")
-        return render(request, 'base.html', {'file_url':files,'urlu':h.text.url})
+#         g=Register.objects.create(text="specs/hesjtigs.txt")
+#         h=Register.objects.get(text="specs/hesjtigs.txt")
+#         print(h.text.url)
+#         with open("media/specs/hesjt.txt", 'r', encoding='utf-8') as openfile:
+#             line = openfile.read()
+#             doc.add_paragraph(line)
+#             doc.save('file' + ".docx")
+#         return render(request, 'base.html', {'file_url':files,'urlu':h.text.url})
 
     return render(request,"index.html",)
