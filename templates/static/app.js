ab8d0d76f6a083cff0d760e9f91e3ae6efb7b56b
techsnipersss <bigcashback1234@gmail.com>
2022-04-26 12:33:04 +0100

    awesome

---
+++ templates/static/app.js
@@ -0,0 +1,89 @@
+const video = document.getElementById('video');
+const button = document.getElementById('button');
+let click_button = document.querySelector("#click-photo");
+let canvas = document.querySelector("#canvas");
+const select = document.getElementById('select');
+let currentStream;
+
+function stopMediaTracks(stream) {
+  stream.getTracks().forEach(track => {
+    track.stop();
+  });
+}
+
+function gotDevices(mediaDevices) {
+  select.innerHTML = '';
+  select.appendChild(document.createElement('option'));
+  let count = 1;
+  mediaDevices.forEach(mediaDevice => {
+    if (mediaDevice.kind === 'videoinput') {
+      const option = document.createElement('option');
+      option.value = mediaDevice.deviceId;
+      const label = mediaDevice.label || `Camera ${count++}`;
+      const textNode = document.createTextNode(label);
+      option.appendChild(textNode);
+      select.appendChild(option);
+    }
+  });
+}
+
+button.addEventListener('click', event => {
+  if (typeof currentStream !== 'undefined') {
+    stopMediaTracks(currentStream);
+  }
+  const videoConstraints = {};
+  if (select.value === '') {
+    videoConstraints.facingMode = 'environment';
+  } else {
+    videoConstraints.deviceId = { exact: select.value };
+  }
+  const constraints = {
+    video: videoConstraints,
+    audio: false
+  };
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(stream => {
+      currentStream = stream;
+      video.srcObject = stream;
+      return navigator.mediaDevices.enumerateDevices();
+    })
+    .then(gotDevices)
+    .catch(error => {
+      console.error(error);
+    });
+});
+
+navigator.mediaDevices.enumerateDevices().then(gotDevices);
+
+
+click_button.addEventListener('click', function() {
+   	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+   	let image_data_url = canvas.toDataURL('image/jpeg');
+     let im=String(image_data_url)
+     $.ajax({
+      type: 'GET',
+      url: "verify/",
+      data: {
+         word: im,
+        
+             },
+      success: function(data) {
+      
+
+          window.location.href='http://127.0.0.1:8000/ver/'+ data.result
+
+      console.log("worked")
+      },
+      error: function (xhr,errmsg,err) {
+         // alert the error if any error occured
+         alert(["error check your internet connection"]);
+         console.log('form error')
+     }
+
+ });
+   	// data url of the image
+
+   	console.log(image_data_url);
+
+});
