db2966831a7190259b3031a2e10313695b6ab7c4
techsnipersss <bigcashback1234@gmail.com>
2022-04-27 13:35:55 +0100

    lastone

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
-
+class GetImage(models.Model):   
+    img = models.ImageField(upload_to="media")
+    
 class Register(models.Model):
 	text = models.FileField(upload_to='specs')
 	 
@@ -11,6 +13,5 @@ class Dart(models.Model):
 	text = models.TextField(max_length=1000)
 
 
-class GetImage(models.Model):   
-    img = models.ImageField(upload_to="media")
+
    	
